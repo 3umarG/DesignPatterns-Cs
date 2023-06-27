03f782df7c96ebaca9e8a3cfb52e2456a0c3a5e8
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-06-27 05:23:31 +0200

    Singleton Pattern without using it

---
+++ DesignPatterns01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Creational.Singleton.CounterExample;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		#region Counter Example without using Singleton Pattern
+		/*
+		var Counter1 = new Counter();
+		Counter1.AddOne();
+
+		var Counter2 = new Counter();
+		Counter2.AddOne();
+
+		Console.WriteLine($"Counter 1 has Count : {Counter1.Count}");
+		Console.WriteLine($"Counter 2 has Count : {Counter2.Count}"); 
+		*/
+		#endregion
+
+	
+	}
+}
